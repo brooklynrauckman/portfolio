6590cfd813c40dc3678031b9cc72031b59f2892b
Brooklyn Rauckman <brooklyn.rauckman@gmail.com>
2020-07-15 17:42:13 -0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -11,16 +11,9 @@ function About() {
         <p css={styles.aboutParagraph}>
           So you might as well learn a bit more about me. My passion for math,
           solving problems, and creating things, led me to learn software
-          engineering. I enjoy both collaborating with people and zoning-in on
-          my work. I love the satisfaction of completing tasks, big and small. I
+          engineering. I
           am highly organized and love to simplify and optimize my workflow as
-          well as my life. I chose to create projects with a variety of features
-          and tools to continue to build upon my skill set. I have dedicated
-          myself to learning and practicing foundational software engineering
-          skills. Apart from this knowledge, I have experience in various work
-          settings, providing effective communication and collaboration. I am a
-          lifelong learner who looks forward to the opportunity to work with new
-          technologies and new people.
+          well as my life. In my free time, I love to learn, spend time with family and friends, play sports, and indulge in delicious snacks.
         </p>
         <div css={styles.centeredButtons}>
           <a href="/resume.pdf" target="blank" css={styles.resumeLink}>
